be9f0563cf35946dfa76b71fc86f75bfdd5175f2
VitthalGupta <vitthalgupta27@gmail.com>
2023-01-27 01:00:17 +0530

    requirements for linux and windows

---
+++ linux/README.md
@@ -0,0 +1,17 @@
+# Linux
+
+## Package requirements for Linux
+
+* [Cryptography](https://pypi.org/project/cryptography/)
+* [Selenium](https://pypi.org/project/selenium/)
+* [tqdm](https://pypi.org/project/tqdm/)
+
+## Linux Specific Instructions and Troubleshooting
+
+### Tips and further improvments
+
+* Optimize the wait login wait time according to your specific needs.
+
+* We will roll out a feature to automatically optimise the wait time for relogin based on logs.
+
+[EOF]
--- utility/check_connection.py
+++ utility/check_connection.py
@@ -70,3 +70,139 @@ def check_wifi_connection_mac():
                 print("Rechecking in 5 secs")
                 time.sleep(5)
                 check_wifi_connection_mac()
+
+def check_wifi_connection_linux():
+    # Check available connections
+    available_its = []
+    devices = subprocess.check_output(
+        ["nmcli", "dev", "wifi"])
+    devices = devices.decode('ascii')
+    devices = devices.split("\n")
+    for _ in devices:
+        if (_.find("ITS") != -1):
+            available_its.append(_.replace(" ", ""))
+        # print(available_its)
+    #filtering data
+    only_its = []
+    its = []
+    for _ in available_its:
+        its = _.split("-")
+        if its[0] not in only_its:
+            only_its.append(its[0])
+
+    # Printing availble SSID's
+    # print("Available SSID'S: ")
+    # for _ in only_its:
+    #     print(_)
+    if only_its == []:
+        print("No ITS SSID's Found")
+        print("Rechecking in 5 secs")
+        time.sleep(5)
+        check_wifi_connection_linux()
+    else:
+        # Fetching SSID from the var file
+        if len(only_its) >= 1:
+            network_to_connect = only_its[0]
+        # Get preferred network from the var file
+        preferred_network = fetch_var("Preferred network")
+        # print("preferred network : ", preferred_network )
+        if preferred_network in only_its:
+            network_to_connect = preferred_network
+
+        # Connect to the network {networksetup -setairportnetwork en0 <SSID_OF_NETWORK> <PASSWORD>}
+        # only_its in not empty
+        # check if the network is already connected to the prefered network
+        network_connected = subprocess.check_output(
+            ["nmcli", "dev", "wifi"])
+        network_connected = network_connected.decode('ascii')
+        network_connected = network_connected.split("\n")
+        for _ in network_connected:
+            if (_.find("ITS") != -1):
+                network_connected = _.replace(" ", "")
+        # print(network_connected)
+        # if len(network_connected) >= 1:
+        #     for i in network_connected:
+        #         network_connected.append(i.split(':'))
+        # print(network_connected)
+        if preferred_network in network_connected:
+            # print("Already connected to the preferred network: {}".format(
+            #     network_connected[1]))
+            pass
+        else:
+            if len(only_its) != 0:
+                subprocess.check_output(
+                    ['nmcli', 'dev', 'wifi', 'connect', network_to_connect, 'password', 'iiitbbsr'])
+                # print("Connected to : {} ".format(network_to_connect))
+            else:
+                print("No ITS SSID's found")
+                print("Rechecking in 5 secs")
+                time.sleep(5)
+                check_wifi_connection_linux()
+
+def check_wifi_connection_win():
+    # Check available connections
+    available_its = []
+    devices = subprocess.check_output(
+        ["netsh", "wlan", "show", "networks"])
+    devices = devices.decode('ascii')
+    devices = devices.split("\n")
+    for _ in devices:
+        if (_.find("ITS") != -1):
+            available_its.append(_.replace(" ", ""))
+        # print(available_its)
+    #filtering data
+    only_its = []
+    its = []
+    for _ in available_its:
+        its = _.split("-")
+        if its[0] not in only_its:
+            only_its.append(its[0])
+
+    # Printing availble SSID's
+    # print("Available SSID'S: ")
+    # for _ in only_its:
+    #     print(_)
+    if only_its == []:
+        print("No ITS SSID's Found")
+        print("Rechecking in 5 secs")
+        time.sleep(5)
+        check_wifi_connection_win()
+    else:
+        # Fetching SSID from the var file
+        if len(only_its) >= 1:
+            network_to_connect = only_its[0]
+        # Get preferred network from the var file
+        preferred_network = fetch_var("Preferred network")
+        # print("preferred network : ", preferred_network )
+        if preferred_network in only_its:
+            network_to_connect = preferred_network
+
+        # Connect to the network {networksetup -setairportnetwork en0 <SSID_OF_NETWORK> <PASSWORD>}
+        # only_its in not empty
+        # check if the network is already connected to the prefered network
+        network_connected = subprocess.check_output(
+            ["netsh", "wlan", "show", "networks"])
+        network_connected = network_connected.decode('ascii')
+        network_connected = network_connected.split("\n")
+        for _ in network_connected:
+            if (_.find("ITS") != -1):
+                network_connected = _.replace(" ", "")
+        # print(network_connected)
+        # if len(network_connected) >= 1:
+        #     for i in network_connected:
+        #         network_connected.append(i.split(':'))
+        # print(network_connected)
+        if preferred_network in network_connected:
+            # print("Already connected to the preferred network: {}".format(
+            #     network_connected[1]))
+            pass
+        else:
+            if len(only_its) != 0:
+                subprocess.check_output(
+                    ['netsh', 'wlan', 'connect', 'name=', network_to_connect, 'ssid=', network_to_connect, 'interface=Wi-Fi'])
+                # print("Connected to : {} ".format(network_to_connect))
+            else:
+                print("No ITS SSID's found")
+                print("Rechecking in 5 secs")
+                time.sleep(5)
+                check_wifi_connection_win()
